fdf4639875a191f4b330522874fe49e7c24b92b3
Aaron M. de Windt <aaron.dewindt@gmail.com>
2018-02-07 20:48:36 +0100

    qwdwqf

--- site/index.php
+++ site/index.php
@@ -59,8 +59,10 @@
 
 
     <?php
-        $command = "python3.6 milestones_sheet.py"
+        $command = "python3.6 " . getcwd() . "/milestones_sheet.py";
         echo $command;
+        //$output = shell_exec($command);
+        //echo $output;
     ?>
 
     <script>
